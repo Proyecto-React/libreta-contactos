c848ccc262e628caf0219442610b63f774c1c4f9
Analia <cerrudoanalia@gmail.com>
2022-12-15 00:25:17 -0300

    Menu y Footer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import Show from "./components/Show"
-import Create from "./components/Create"
-import Edit from "./components/Edit"
 import './App.css';
 
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Create from "./components/Create"
+import Edit from "./components/Edit"
+import Footer from "./components/Footer"
+import Inicio from "./components/Inicio"
+import Menu from "./components/Menu"
+import Show from "./components/Show"
 
 function App() {
   return (
     <div className="App">
 <BrowserRouter>
+<Menu></Menu>
 <Routes>
-  <Route path="/" element={<Show/>}/>
+  <Route path="/" element={<Inicio/>}/>
+  <Route path="/show" element={<Show/>}/>
   <Route path="/create" element={<Create/>}/>
   <Route path="/edit/:id" element={<Edit/>}/>
 </Routes>
-
+<Footer></Footer>
 </BrowserRouter>
 
     </div>
---
+++ src/components/Inicio.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Inicio=()=>{
+
+    return (
+
+            <div className="inicio">
+                <h1>Libreta de Contacto</h1>
+                <img src="../imagenes/grupo2.jpeg"></img>
+            </div>
+
+    )
+}
+export default Inicio;
