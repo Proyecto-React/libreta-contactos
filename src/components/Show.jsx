fe181ec60ba25cac3b05b223d4e140b456a49e12
Jose Nunez <jose.nunez@sdp.biz>
2022-12-15 11:52:58 -0300

    Added search functionality

--- src/components/Show.jsx
+++ src/components/Show.jsx
@@ -1,115 +1,143 @@
-import { useState,useEffect } from "react";
-import {Link} from "react-router-dom"
-import {collection,getDocs,deleteDoc,doc} from "firebase/firestore"
-import {db} from "../firebaseConfig/firebase.js"
-import Swal from "sweetalert2"
-import withReactContent from "sweetalert2-react-content"
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { collection, getDocs, deleteDoc, doc } from "firebase/firestore";
+import { db } from "../firebaseConfig/firebase.js";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 // eslint-disable-next-line  no-unused-vars
-const MySwal = withReactContent(Swal)
+const MySwal = withReactContent(Swal);
 
+const searchName = () => {
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("myInput");
 
-const Show = ()=>{
-// 1 configurar los hooks
-const [contactos,setContactos]= useState([])
-// 2 referenciamos la db de firestore
-const contactosCollection = collection(db,"contactos")
-//3 funcion para mostrar todos los docs
-const getContactos= async () =>{
-    const data = await getDocs (contactosCollection)
-/*     console.log (data.docs) */
-setContactos(
-    data.docs.map((doc)=>({...doc.data(),id:doc.id}))
-)
-/*     console.log (contactos) */
-}
-// 4 funcion para eliminar un doc
-const deleteContacto= async(id)=>{
-const contactoDoc= doc(db,"contactos",id)
-await deleteDoc (contactoDoc)
-getContactos()
-}
-//5 funcion para la confirmacion de swwet alert
-const confirmDelete=(id)=>{
-    Swal.fire({
-        title: 'Estas Seguro/a',
-        text: "No podes revertir este cambio",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Si, deseo Borrarlo'
-      }).then((result) => {
-        if (result.isConfirmed) {
-            deleteContacto(id) //llamamos a la funcion eliminar
-          Swal.fire(
-            'Borrado!',
-            'Has eliminado el Documento.',
-            'success'
-          )
+    console.log('Buscando: ' + input)
+
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+        if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+        } else {
+        tr[i].style.display = "none";
         }
-      })
-
+    }
+    }
 
 }
-//6 use useEffect
 
-useEffect(()=>{
-    getContactos()
+const Show = () => {
+  // 1 configurar los hooks
+  const [contactos, setContactos] = useState([]);
+  // 2 referenciamos la db de firestore
+  const contactosCollection = collection(db, "contactos");
+  //3 funcion para mostrar todos los docs
+  const getContactos = async () => {
+    const data = await getDocs(contactosCollection);
+    /*     console.log (data.docs) */
+    setContactos(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    /*     console.log (contactos) */
+  };
+  // 4 funcion para eliminar un doc
+  const deleteContacto = async (id) => {
+    const contactoDoc = doc(db, "contactos", id);
+    await deleteDoc(contactoDoc);
+    getContactos();
+  };
+  //5 funcion para la confirmacion de swwet alert
+  const confirmDelete = (id) => {
+    Swal.fire({
+      title: "Estas Seguro/a",
+      text: "No podes revertir este cambio",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, deseo Borrarlo",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteContacto(id); //llamamos a la funcion eliminar
+        Swal.fire("Borrado!", "Has eliminado el Documento.", "success");
+      }
+    });
+  };
+  //6 use useEffect
+
+  useEffect(() => {
+    getContactos();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-},[])
-//7 devolvemos la vista de nuestro componente
-return(
- // fragment
- <> 
-<div className="container">
-<div className="row">
-    <div className="col">
-        <div className="d-grid gap-2">
-<Link to="/create" className="btn btn-secondary mt-2 mb-2">CREAR</Link>
+  }, []);
+  //7 devolvemos la vista de nuestro componente
+  return (
+    // fragment
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="d-grid gap-2">
+              <Link to="/create" className="btn btn-secondary mt-2 mb-2">
+                CREAR
+              </Link>
+            </div>
+            <div>
+            <form id="form">
+                <input
+                  type="text"
+                  id="myInput"
+                  onKeyUp={searchName} 
+                  placeholder="Search for names.."
+                  title="Type in a name"
+                />
+              </form>
+            </div>
+            <table className="table table-dark table-hover" id="myTable">
+              <thead>
+                <tr>
+                  <th>Nombre</th>
+                  <th>Apellido</th>
+                  <th>Email</th>
+                  <th>Teléfono</th>
+                  <th>Acciones</th>
+                </tr>
+              </thead>
+              <tbody>
+                {contactos.map((contacto) => (
+                  <tr key={contacto.id}>
+                    <td>{contacto.nombre}</td>
+                    <td>{contacto.apellido}</td>
+                    <td>{contacto.email}</td>
+                    <td>{contacto.telefono}</td>
+                    <td>
+                      <Link
+                        to={`/edit/${contacto.id}`}
+                        className="btn btn-light"
+                      >
+                        <i className="fa-solid fa-pencil"></i>
+                      </Link>
+                      <button
+                        onClick={() => {
+                          confirmDelete(contacto.id);
+                        }}
+                        className="btn btn-danger"
+                      >
+                        <i className="fa-solid fa-trash"></i>
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-<table className="table table-dark table-hover">
-<thead>
-    <tr>
-        <th>Nombre</th>
-        <th>Apellido</th>
-        <th>Email</th>
-        <th>Teléfono</th>
-        <th>Acciones</th>        
-    </tr>
-</thead>
-<tbody>
-{contactos.map((contacto)=>(
-<tr key={contacto.id}>
-    <td>{contacto.nombre}</td>
-    <td>{contacto.apellido}</td>
-    <td>{contacto.email}</td>
-    <td>{contacto.telefono}</td>
-    <td>
-        <Link to={`/edit/${contacto.id}`} className="btn btn-light" ><i className="fa-solid fa-pencil"></i></Link>
-    <button onClick={()=>{confirmDelete(contacto.id)}} className="btn btn-danger"><i className="fa-solid fa-trash"></i></button>
-    </td>
-</tr>
+      </div>
+    </>
+  );
+};
 
-
-))}
-
-</tbody>
-
-
-</table>
-    </div>  
-
-</div>
-
-
-</div>
-
-</>
-)
-
-}
-
-export default Show
-
-
+export default Show;
